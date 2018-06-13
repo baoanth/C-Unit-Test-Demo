5ea90bf24ad78459d7816662f3fbb180ae9c157c
Jakob Wanner <jakobwanner@gmx.de>
2018-06-13 14:16:58 +0200

    Added Switcher test + SwitchableMock

--- tests/my_lib/MyLib_AllTests.cmake
+++ tests/my_lib/MyLib_AllTests.cmake
@@ -8,4 +8,6 @@ set(MYLIB_UNIT_TESTS_SOURCE_FILES
     ${MYLIB_UNIT_TESTS_ROOT}/testA.cpp
     ${MYLIB_UNIT_TESTS_ROOT}/testRadio.cpp
     ${MYLIB_UNIT_TESTS_ROOT}/testMicrowave.cpp
+    ${MYLIB_UNIT_TESTS_ROOT}/testSwitcher.cpp
+    ${MYLIB_UNIT_TESTS_ROOT}/SwitchableMock.c
 )
